21c9c7f0afc15dcbd266425afa4db2406fd984c7
Kuba-Dvorak <jakub.dvorak.025@pslib.cz>
2026-02-25 16:11:07 +0100

    a_lot_of_localization_1

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const myCanvas = document.querySelector('#MyCanvas')
 const drawer = myCanvas.getContext('2d')
-const blocksDown = []
 const mySlider = document.querySelector('#MySlider')
+let blocksDown = []
+let fallingBlocks = []
 
 function absoluteDistance(target_A,target_B){
     let myA = target_A
@@ -11,35 +12,81 @@ function absoluteDistance(target_A,target_B){
     return Math.sqrt(Math.pow(dx,2)+Math.pow(dy,2))
 }
 
+function eliminateLostBlocks(blocksList){
+    let newList = []
+    for (let elBlock of blocksList){
+        if (!elBlock.stopped){
+            newList.push(elBlock)
+        }
+    }
+    return newList
+}
+
+function leaveNotCurrentID(ourblockslist,id){
+    let ournewList = []
+    for (let elBlock of ourblockslist){
+        if (!(elBlock.id === id)){
+            ournewList.push(elBlock)
+        }
+    }
+    return ournewList
+}
+
+function CurrentBlocksLenght(ourblockslist){
+    let ourNum = 0
+    for (let elBlock of ourblockslist){
+        if (elBlock.current){
+            ourNum += 1
+        }
+    }
+    return ourNum
+}
+
 
 
 class Block{
-    constructor() {
+    constructor(currentblock,x,y,id) {
+        this.id = id
         this.size_x = 100
         this.size_y = 100
         this.stopped = false
-        this.x = myCanvas.width/2
-        this.y = 0
+        this.x = x
+        this.y = y
         this.innerRad = this.size_x/2
         this.outerRad = Math.sqrt(Math.pow(this.size_x,2)+Math.pow(this.size_y,2))
         this.centre = [this.x+this.size_x/2,this.y+this.size_y/2]
         this.left = null
         this.right = null
+        this.current = currentblock
+        this.makeNewBlocklocc = [true,-10]
     }
     colisions() {
        for (let blocker of blocksDown){
         if (!blocker.stopped){
-            let variable1 = absoluteDistance(blocker.centre,this.centre)
-            if (variable1<(this.outerRad+blocker.outerRad)) {
-                if (variable1<(this.innerRad+blocker.innerRad)){
-                    return [true, blocker.y]
-                }
+            if (absoluteDistance(blocker.centre,this.centre)<(this.innerRad+blocker.innerRad)) {
+                //console.log(`Blok ID: ${this.id} kolize s ${blocker.id}`)
+                return [true, blocker.y]
             }}
         }
        if (this.y+this.size_y >= myCanvas.height){
+         //console.log(`Blok ID: ${this.id} kolize s zemi`)
          return [true, myCanvas.height]
        }
        return [false, -10]
+    }
+    collision_expept(change_x){
+        let local_centre = this.centre
+        local_centre[0] += change_x
+        for (let blocker of blocksDown){
+        if (!blocker.stopped){
+            if (absoluteDistance(blocker.centre,local_centre)<(this.innerRad+blocker.innerRad)) {
+                return true
+            }}
+        }
+       if (this.y+this.size_y >= myCanvas.height){
+         return true
+       }
+       return false
     }
     updateblock(difficulty) {
         this.y += difficulty
@@ -47,14 +94,12 @@ class Block{
     }
     stableSetup() {
         for (let blocker of blocksDown){
-            if (blocker.y-10<=this.y && this.y<=blocker.y+10){
-                if (blocker.x-10<=this.x+100 && this.x+100<=blocker.x+10){
-                    console.log('colision')
+            if (blocker.centre[1]-blocker.innerRad<=this.centre[1] && blocker.centre[1]+blocker.innerRad>=this.centre[1]){
+                if (blocker.centre[0]-blocker.innerRad<=this.centre[0]+this.outerRad && blocker.centre[0]+blocker.innerRad>=this.centre[0]+this.outerRad){
                     this.right = blocker
                     blocker.left = this
                 }
-                if (blocker.x-10<=this.x-100 && this.x-100<=blocker.x+10){
-                    console.log('colision')
+                if (blocker.centre[0]-blocker.innerRad<=this.centre[0]-this.outerRad && blocker.centre[0]+blocker.innerRad>=this.centre[0]-this.outerRad){
                     this.left = blocker
                     blocker.right = this
                 }
@@ -62,11 +107,9 @@ class Block{
         }
          if (this.centre[0]-this.outerRad<=0){
             this.left = 'left'
-            console.log('left')
         }
         if (this.centre[0]+this.outerRad>=myCanvas.width){
             this.right = 'right'
-            console.log('right')
         }
     }
     checkRow(){
@@ -85,78 +128,117 @@ class Block{
             }
         }
         this.stopped = true
-        this.y = 0
+        this.y = -50
         this.x = -1000
+    }
+    updatemyself(){
+        let actualSliderValue = parseFloat(mySlider.value)
+        this.updateblock(actualSliderValue)
+        this.makeNewBlocklocc = this.colisions()
+            
+        if (this.makeNewBlocklocc[0]){
+            this.y = this.makeNewBlocklocc[1]-this.size_y
+            if (this.y<50){
+                stopping = true
+                drawer.fillStyle = 'blue'
+                drawer.fillRect(0, 0, 200, 150)
+                //here will be a lose screen information
+            }
+            this.stableSetup()
+            blocksDown.push(this)
+            if (this.current){
+                startnewGen -= 1
+            }
+            this.current = false
+            fallingBlocks = leaveNotCurrentID(fallingBlocks,this.id)
+            if (startnewGen <= 0){
+                fallingBlocks.push(new Block(true,myCanvas.width/2,0,ids++))
+                fallingBlocks.push(new Block(true,myCanvas.width/2+100,0,ids++))
+                //fallingBlocks.push(new Block(true,myCanvas.width/2-100,0,ids++))
+                //fallingBlocks.push(new Block(true,myCanvas.width/2+200,0,ids++))
+                startnewGen = CurrentBlocksLenght(fallingBlocks)
+            
+        }
+        }
     }
 }
 
-
-let makeNewBlock = [true,-10]
-let started = true
-let myBlock
+let ids = 0
 let stopping = false
+fallingBlocks.push(new Block(true,myCanvas.width/2,0,ids++))
+let startnewGen = CurrentBlocksLenght(fallingBlocks)
 
 
 function main() {
-    let actualSliderValue = parseFloat(mySlider.value)
+    drawer.fillStyle = 'red'
+    drawer.strokeStyle = 'black'
     if (!stopping){
         drawer.clearRect(0, 0, myCanvas.width, myCanvas.height)
-        if (!started){
-                myBlock.updateblock(actualSliderValue)
-                makeNewBlock = myBlock.colisions()
-            }
-        if (makeNewBlock[0]){
-            if (!started){
-                myBlock.y = makeNewBlock[1]-myBlock.size_y
-                if (myBlock.y<50){
-                    stopping = true
-                    drawer.fillStyle = 'blue'
-                    drawer.fillRect(0, 0, 200, 150)
-                    //here will be a lose screen information
-                }
-                myBlock.stableSetup()
-                blocksDown.push(myBlock)
-            }
-            myBlock = new Block
-            started = false
+        for (let fallers of fallingBlocks){
+            fallers.updatemyself()
+            drawer.fillRect(fallers.x, fallers.y, fallers.size_x, fallers.size_y)
+            drawer.strokeRect(fallers.x, fallers.y, fallers.size_x, fallers.size_y)
         }
-        drawer.fillStyle = 'red'
-        drawer.strokeStyle = 'black'
         for (let blocky of blocksDown){
             if (!blocky.stopped){
                 drawer.fillRect(blocky.x, blocky.y, blocky.size_x, blocky.size_y)
-                drawer.strokeRect(blocky.x, blocky.y, blocky.size_x, myBlock.size_y)
+                drawer.strokeRect(blocky.x, blocky.y, blocky.size_x, blocky.size_y)
                 if (blocky.left === 'left'){
                 if (blocky.checkRow()){
                     blocky.turnOffRow()
+                    blocksDown = eliminateLostBlocks(blocksDown)
+                    fallingBlocks = fallingBlocks.concat(blocksDown)
+                    blocksDown = []
                     for (let blackers of blocksDown){
                         if (!blackers.stopped){
-                            blackers.y += 100
+                            blackers.updateblock(100)
                         }
                     }
                 }
             }
             }
         }
-        drawer.fillRect(myBlock.x, myBlock.y, myBlock.size_x, myBlock.size_y)
-        drawer.strokeRect(myBlock.x, myBlock.y, myBlock.size_x, myBlock.size_y)
-        requestAnimationFrame(main)
     }
+    requestAnimationFrame(main)
 }
 
 window.addEventListener('keydown', (event) => {
     if (event.key === 'ArrowRight') {
-        if (!started){
-            if (myBlock.x<=myCanvas.width-myBlock.size_x*2){
-                myBlock.x += 100
+        let fallNum = 0
+        for (let fallers of fallingBlocks){
+            if (fallers.x<=myCanvas.width-fallers.size_x*2 && !fallers.collision_expept(100) && fallers.current){
+                fallNum += 1
             }
+        }
+        console.log(CurrentBlocksLenght(fallingBlocks))
+        console.log(fallNum)
+        if (fallNum>=CurrentBlocksLenght(fallingBlocks)){
+            for (let fallers of fallingBlocks){
+                if (fallers.x<=myCanvas.width-fallers.size_x*2 && !fallers.collision_expept(100) && fallers.current){
+                    fallers.x += 100
+                }
+            }
+            fallNum = 0
         }
     }
     if (event.key === 'ArrowLeft') {
-        if (myBlock.x>=0+myBlock.size_x){
-                myBlock.x -= 100
+        let fallNum = 0
+        for (let fallers of fallingBlocks){
+            if (fallers.x>=fallers.size_x && !fallers.collision_expept(-100) && fallers.current){
+                fallNum += 1
             }
-    }
+        }
+        console.log(CurrentBlocksLenght(fallingBlocks))
+        console.log(fallNum)
+        if (fallNum>=CurrentBlocksLenght(fallingBlocks)){
+            for (let fallers of fallingBlocks){
+                if (fallers.x>=fallers.size_x && !fallers.collision_expept(100) && fallers.current){
+                    fallers.x -= 100
+                }
+            }
+            fallNum = 0
+        }
+}
 });
 
 main()
